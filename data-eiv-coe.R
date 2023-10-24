d1b781e28f9c3638d5e66e1f61f10db0c64e0883
erkind <erkindoglu@gmail.com>
2023-10-24 20:52:33 +0200

    Create data-eiv-coe.R

---
+++ data-eiv-coe.R
@@ -0,0 +1,50 @@
+# eiv-coe data
+#
+# # loading from hard drive, mind customizing the path
+# data <- read.csv(paste("c:/users/68596/",
+#                        "Dropbox/eiv-coe/data/",
+#                        "data-eiv-coe.csv", sep = ""),
+#                  header = TRUE)
+# loading from GitHub
+data <- read.csv(paste("http://raw.githubusercontent.com/erkind/",
+                       "eiv-coe/main/data-eiv-coe.csv",
+                       sep = ""),
+                 header = TRUE)
+dim(data)
+colnames(data)
+head(data[c(1:3,51:53,57:59)], n = 2)
+tail(data[c(1:3,51:53,57:59)], n = 2)
+#
+# set sample period
+t.start<- as.numeric(which(data$date == 199011))
+t.end  <- as.numeric(which(data$date == 202202))
+#
+# Rf rate
+RF <- as.matrix(data[c(t.start:t.end), 56],
+                nrow = t.end - t.start + 1,
+                ncol = 1)
+colnames(RF) <- c("RF")
+dim(RF); colnames(RF)
+# excess returns on industry portfolios
+R <- as.matrix(data[c(t.start:t.end),2:45], # exclude financials
+               nrow = t.end - t.start + 1,
+               ncol = 44)
+R <- R - data[c(t.start:t.end), 56] # subtract RF
+dim(R); colnames(R)
+#
+# factors
+F1 <- as.matrix(data[c(t.start:t.end),51],
+                nrow = t.end - t.start + 1,
+                ncol = 1)
+colnames(F1) <- colnames(data)[51]
+F3 <- as.matrix(data[c(t.start:t.end),51:53],
+                nrow = t.end - t.start + 1,
+                ncol = 3)
+F4 <- as.matrix(data[c(t.start:t.end),c(51:53, 61)],
+                nrow = t.end - t.start + 1,
+                ncol = 4)
+F5 <- as.matrix(data[c(t.start:t.end),51:55],
+                nrow = t.end - t.start + 1,
+                ncol = 5)
+#
+rm("t.start", "t.end")
