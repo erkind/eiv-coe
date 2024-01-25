684a00bba439647a392e2f65c7ae0cdee12b3be7
erkind <erkindoglu@gmail.com>
2024-01-25 21:13:42 +0100

    updates

--- g-f-HM-Zmat.R
+++ g-f-HM-Zmat.R
@@ -1,22 +1,22 @@
-# Calculating Z-matrix of higher-moments instruments
+# calculating Z-matrix of higher-moments instruments
 #
 generate.Z.mat <- function(
-      X.mat,		# n by k matrix of independent variables
-      Y.mat,		# n by 1 vector of dependent variable
-      n,          # sample size 
-      k,          # nb of predictors
-      i = c(1, 4) # list of higher-moments
+      X.mat,		  # n by k matrix of independent variables
+      Y.vec = NULL, # n by 1 vector of dependent variable
+      n,            # sample size 
+      k,            # nb of predictors
+      i = c(1, 4)   # list of higher-moments
       )
    {
    #
    # nb of cols in X.mat must be equal to k
-   if(k != dim(X.mat)[2]){
+   if(k != dim(as.matrix(X.mat))[2]){
       stop("The number of columns in X.mat must be equal to k.")
    }
    #
    # calculate Y.vec and X.mat in mean deviations form
-   y.vec <- apply(as.data.frame(Y.vec), 2, function(a){return(a - mean(a))})
-   x.mat <- apply(as.data.frame(X.mat), 2, function(a){return(a - mean(a))})
+   y.vec <- scale(Y.vec, scale = FALSE)
+   x.mat <- scale(X.mat, scale = FALSE)
    #
    # calculate higher-moment instruments
    # z0 = unit vector
@@ -52,11 +52,9 @@ generate.Z.mat <- function(
       }
       part2 <- matrix(0, nrow = n, ncol = ncol(x.mat))
       part2 <- 2 * x.mat %*% (mean(t(x.mat) %*% y.vec) * diag(ncol(x.mat)))
-      # part2 <- 2 * x.mat %*% (mean(t(x.mat) %*% y.vec) * diag(k))
       part3 <- matrix(0, nrow = n, ncol = ncol(x.mat))
       part3 <- y.vec %*% t(rep(1, ncol(x.mat))) %*%
          (mean((t(x.mat) %*% x.mat) / n) * diag(k))
-      # part3 <- y.vec * sum(mean(t(x.mat) %*% x.mat / n) * diag(k))
       my.value <- part1 - part2 - part3
       Z.mat <- cbind(Z.mat, my.value)
    }
@@ -76,15 +74,24 @@ generate.Z.mat <- function(
    #
    # Z.mat column labels
    #
-   # define valid ranges for i and j
-   valid_i <- 1:7
-   valid_k <- 1:5
+   # define valid range for i
+   i.range <- 1:7
+   # i values consistent with the nb of columns in Z.mat, i in 1:7
+   # 
+   # define valid range for k
+   k.range <- 1:5
+   # k values consistent with the max nb of regressors among different
+   # specifications. the default value set as 5 to handle the FF 5-factor
+   # model.
+   # if k > 5, the user should add additional lines after the command line
+   # beginning with " if(k == 5){... " below.
+   #
    # check if the values of i and k are within the valid ranges
-   if(any(!i %in% valid_i)){
+   if(any(!i %in% i.range)){
       stop("Invalid value(s) for i
            This should be an integer between 1 and 7.")
    }
-   if (!(k %in% valid_k)) {
+   if (!(k %in% k.range)) {
       stop("Invalid value for k.
            It should be an integer between 1 and 5.")
    }
@@ -96,31 +103,31 @@ generate.Z.mat <- function(
    labels <- c(labels, "z0")
    #
    # if k is 1, add additional labels for 1:1 for each value of i
-   if (k == 1){
+   if(k == 1){
       for (ii in i){
          labels <- c(labels, paste0("z", ii, 1:1))
       }
    }
    # if k is 2, add additional labels for 1:2 for each value of i
-   if (k == 2){
+   if(k == 2){
       for (ii in i){
          labels <- c(labels, paste0("z", ii, 1:2))
       }
    }
    # if k is 3, add additional labels for 1:3 for each value of i
-   if (k == 3){
+   if(k == 3){
       for (ii in i){
          labels <- c(labels, paste0("z", ii, 1:3))
       }
    }
    # if k is 4, add additional labels for 1:4 for each value of i
-   if (k == 4){
+   if(k == 4){
       for (ii in i){
          labels <- c(labels, paste0("z", ii, 1:4))
       }
    }
    # if k is 5, add additional labels for 1:5 for each value of i
-   if (k == 5){
+   if(k == 5){
       for (ii in i){
          labels <- c(labels, paste0("z", ii, 1:5))
       }
@@ -132,5 +139,3 @@ generate.Z.mat <- function(
    # return Z.mat
    return(Z.mat)
 }
-
-
