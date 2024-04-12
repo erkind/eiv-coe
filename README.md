6c6489ea1749dd654eb49bdcdf1f630b5b941e2d
erkind <73018025+erkind@users.noreply.github.com>
2024-04-12 23:22:00 +0200

    updates
    updates for cascad project

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
-Project repository
-
-Project title:
+TITLE
 Higher-Moment Estimation of Industry-level Cost of Equity Capital with Errors-in-variables
 
-File descriptions:
+DESCRIPTION
+Public repository for reproducible results of the paper
+
+SYSTEM
+All scripts have been run on an RStudio 2022.02.3+492 Prairie Trillium Release using R version 4.2.1 installed on a Windows 10 Intel(R) Core(TM) processor i5-4310U CPU 2.00GHz 2.60GHz 64bits x64 system
+
+REQUIRED PACKAGES
+car
+dplyr
+fbasics
+lmtest
+sandwich
+skedastic
+
+USAGE
+g-coe-data-eiv.R
+The file loads the original dataset and sets the subsample employed in the paper. It declares "R" the matrix of industry portfolio excess returns over the risk-free rate and "X" the matrix of systematic risk factors.
+
+
+
+
 
--- g-coe-data-eiv.R
+++ g-coe-data-eiv.R
@@ -1,40 +1,42 @@
 # eiv-coe data
 #
-# loading raw csv file
-data <- read.csv(paste("C:/Users/68596/Dropbox/",
-                       "eiv-coe/data/data-eiv-coe.csv", sep = ""),
+# loading raw data
+data <- read.csv(paste("http://raw.githubusercontent.com/erkind/",
+                       "eiv-coe/main/data-eiv-coe.csv",
+                       sep = ""),
                  header = TRUE, sep = ",")
-# data <- read.csv(paste("http://raw.githubusercontent.com/erkind/",
-#                        "eiv-coe/main/data-eiv-coe.csv",
-#                        sep = ""),
-#                  header = TRUE, sep = ",")
+#
+# checking data
 dim(data)
 colnames(data)
 head(data[c(1:3,51:53,57:59)], n = 2)
 tail(data[c(1:3,51:53,57:59)], n = 2)
 #
-# set sample period
+# set sample period to 199011-202202
 t.start<- as.numeric(which(data$date == 199011))
 t.end  <- as.numeric(which(data$date == 202202))
 #
-# risk-free rate
+# vector of risk-free rate
 RF <- as.matrix(data[c(t.start:t.end), 56],
                 nrow = t.end - t.start + 1,
                 ncol = 1)
 colnames(RF) <- c("RF")
-dim(RF); colnames(RF)
+dim(RF)
+colnames(RF)
 #
-# excess returns on industry portfolios
+# matrix of excess returns on industry portfolios
 R <- as.matrix(data[c(t.start:t.end),2:45], # exclude financials
                nrow = t.end - t.start + 1,
                ncol = 44)
 R <- R - data[c(t.start:t.end), 56] # subtract RF
-dim(R); colnames(R)
+dim(R)
+colnames(R)
 #
-# factors
+# matrix of risk factors
 X <- as.matrix(data[c(t.start:t.end), c(51:55,61)],
                nrow = t.end - t.start + 1,
                ncol = 6)
 dim(X); colnames(X)
 #
+# remove unnecessary objects
 rm("t.start", "t.end")
