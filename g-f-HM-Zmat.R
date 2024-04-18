0e12469d9c21797d20e068bc9a1b2b632a3d7a02
erkind <73018025+erkind@users.noreply.github.com>
2024-04-18 17:58:34 +0200

    Update g-f-HM-Zmat.R
    function Z-mat corrected cascad

--- g-f-HM-Zmat.R
+++ g-f-HM-Zmat.R
@@ -1,11 +1,11 @@
 # calculating Z-matrix of higher-moments instruments
 #
 generate.Z.mat <- function(
-      X.mat,		      # n by k matrix of independent variables
-      Y.vec = NULL,     # n by 1 vector of dependent variable
-      n,                # sample size 
-      k,                # nb of predictors
-      iv.list = c(1, 4) # list of higher-moments
+      X.mat,		  # n by k matrix of independent variables
+      Y.vec = NULL, # n by 1 vector of dependent variable
+      n,            # sample size 
+      k,            # nb of predictors
+      i = c(1, 4)   # list of higher-moments
 )
 {
    #
@@ -19,139 +19,126 @@ generate.Z.mat <- function(
    x.mat <- scale(X.mat, scale = FALSE)
    #
    # calculate higher-moment instruments
-   #
-   # z0
-   # calculate z0 = vector of ones
-   Z.mat <- matrix(rep(1, n), nrow = n, ncol = 1)
-   # label z0
-   colnames(Z.mat) <- "z0"
-   #
-   # column labels index for additional instruments
-   col.index <- 1
-   #
+   # z0 = unit vector
+   Z.mat <- rep(1, n, 1)
    # z1
-   if(is.element(1, iv.list)){
-      # calculate z1
-      z1 <- x.mat * x.mat
-      # append Z.mat
-      Z.mat <- cbind(Z.mat, z1)
-      # label z1
-      new.cols <- paste0("z1", 1:k)
-      colnames(Z.mat)[(col.index + 1):(col.index + k)] <- new.cols
-      col.index <- col.index + k
+   if(is.element(1, i)){
+      Z.mat <- cbind(Z.mat, x.mat * x.mat)
    }
    # z2
-   if(is.element(2, iv.list)){
-      # calculate z2
-      z2 <- matrix(0, nrow = n, ncol = k)
-      for(j in 1:k){
-         z2[,j] <- x.mat[,j] * y.vec
+   if(is.element(2, i)){
+      my.value <- matrix(0, nrow = n, ncol = ncol(x.mat))
+      for(j in 1:ncol(x.mat)){
+         my.value[,j] <- x.mat[,j] * y.vec
       }
-      # append Z.mat
-      Z.mat <- cbind(Z.mat, z2)
-      # label z2
-      new.cols <- paste0("z2", 1:k)
-      colnames(Z.mat)[(col.index + 1):(col.index + k)] <- new.cols
-      col.index <- col.index + k
+      Z.mat <- cbind(Z.mat, my.value)
    }
    # z3
-   if((is.element(3, iv.list))){
-      # calculate z3
-      z3 <- y.vec * y.vec
-      # append Z.mat
-      Z.mat <- cbind(Z.mat, z3)
-      # label z3
-      colnames(Z.mat)[col.index + 1] <- "z31"
-      col.index <- col.index + 1
+   if((is.element(3, i))){
+      Z.mat <- cbind(Z.mat, y.vec * y.vec)
    }
    # z4
-   if((is.element(4, iv.list))){
-      # calculate z4
-      # z4.1 and z4.2
-      z4.1 <- x.mat * x.mat * x.mat
-      z4.2 <- 3 * x.mat %*% (((t(x.mat) %*% x.mat) / n) * diag(k))
-      #
-      z4 <- z4.1 - z4.2
-      # append Z.mat
-      Z.mat <- cbind(Z.mat, z4)
-      # label z4
-      new.cols <- paste0("z4", 1:k)
-      colnames(Z.mat)[(col.index + 1):(col.index + k)] <- new.cols
-      col.index <- col.index + k
+   if((is.element(4, i))){
+      part1 <- x.mat * x.mat * x.mat
+      part2 <- 3 * x.mat %*% (mean((t(x.mat) %*% x.mat) / n) * diag(k))
+      my.value <- part1 - part2
+      Z.mat <- cbind(Z.mat, my.value)
    }
    # z5
-   if((is.element(5, iv.list))){
-      # calculate z5
-      # z5.1
-      z5.1 <- matrix(0, nrow = n, ncol = k)
-      for(j in 1:k){
-         z5.1[,j] <- x.mat[,j] * x.mat[,j] * y.vec
+   if((is.element(5, i))){
+      #
+      part1 <- matrix(0, nrow = n, ncol = ncol(x.mat))
+      for(j in 1:ncol(x.mat)){
+         part1[,j] <- x.mat[,j] * x.mat[,j] * y.vec
       }
-      # z5.2
-      z5.2 <- 2 * (x.mat %*% 
-                      diag(
-                         as.vector(
-                            (1/n) * (t(x.mat) %*% y.vec)
-                         ),
-                         nrow = k, ncol = k
-                      )
-      )
-      # z5.3
-      # z5.3.1 and z5.3.2
-      z5.3.1 <- matrix(1, nrow = k, ncol = 1)
-      z5.3.2 <- ((t(x.mat) %*% x.mat) / n) * diag(k)
+      part2 <- matrix(0, nrow = n, ncol = ncol(x.mat))
+      part2 <- 2 * x.mat %*% (mean(t(x.mat) %*% y.vec) * diag(ncol(x.mat)))
       # 
-      z5.3 <- y.vec %*% (t(z5.3.1) %*% z5.3.2)
+      part3 <- matrix(0, nrow = n, ncol = ncol(x.mat))
+      part3 <- y.vec %*% t(rep(1, ncol(x.mat))) %*%
+         (mean((t(x.mat) %*% x.mat) / n) * diag(k))
       # 
-      z5 <- z5.1 - z5.2 - z5.3
-      # append Z.mat
-      Z.mat <- cbind(Z.mat, z5)
-      # label z5
-      new.cols <- paste0("z5", 1:k)
-      colnames(Z.mat)[(col.index + 1):(col.index + k)] <- new.cols
-      col.index <- col.index + k
+      my.value <- part1 - part2 - part3
+      Z.mat <- cbind(Z.mat, my.value)
    }
    # z6
-   if((is.element(6, iv.list))){
-      # calculate z6
-      # z6.1
-      z6.1 <- matrix(0, nrow = n, ncol = k)
-      for(j in 1:k){
-         z6.1[,j] <- x.mat[,j] * y.vec * y.vec
-      }
-      # z6.2
-      scalar <- (1/n) * (t(y.vec) %*% y.vec)
-      z6.2 <- x.mat * as.numeric(scalar)
-      # z6.3
-      z6.3 <- 2 * (
-         y.vec %*% ((1/n) * (t(y.vec) %*% x.mat))
-      )
-      #
-      z6 <- z6.1 - z6.2 - z6.3
-      # append Z.mat
-      Z.mat <- cbind(Z.mat, z6)
-      # label z6
-      new.cols <- paste0("z6", 1:k)
-      colnames(Z.mat)[(col.index + 1):(col.index + k)] <- new.cols
-      col.index <- col.index + k
+   if((is.element(6, i))){
+      part1 <- x.mat * y.vec * y.vec
+      part2 <- x.mat * mean((t(y.vec) %*% y.vec / n))
+      part3 <- 2 * y.vec * mean((t(y.vec) %*% x.mat / n))
+      my.value <- part1 - part2 - part3
+      Z.mat <- cbind(Z.mat, my.value)
    }
    # z7
-   if((is.element(7, iv.list))){
-      # calculate z7
-      z7 <- matrix(0, nrow = n, ncol = 1)
-      #
-      z7.1 <- y.vec * y.vec * y.vec
-      #
-      scalar <- (1/n) * (t(y.vec) %*% y.vec)
-      z7.2 <- 3 * y.vec * as.numeric(scalar)
-      #
-      z7 <- z7.1 - z7.2
-      # append Z.mat
-      Z.mat <- cbind(Z.mat, z7)
-      # label z7
-      colnames(Z.mat)[col.index + 1] <- "z71"
-      col.index <- col.index + 1
+   if((is.element(7, i))){
+      my.value <- y.vec * y.vec * y.vec - 3 * y.vec * mean(t(y.vec) %*% y.vec / n)
+      Z.mat <- cbind(Z.mat, my.value)
    }
    #
+   # Z.mat column labels
+   #
+   # define valid range for i
+   i.range <- 1:7
+   # i values consistent with the nb of columns in Z.mat, i in 1:7
+   # 
+   # define valid range for k
+   k.range <- 1:5
+   # k values consistent with the max nb of regressors among different
+   # specifications. the default value set as 5 to handle the FF 5-factor
+   # model.
+   # if k > 5, the user should add additional lines after the command line
+   # beginning with " if(k == 5){... " below.
+   #
+   # check if the values of i and k are within the valid ranges
+   if(any(!i %in% i.range)){
+      stop("Invalid value(s) for i
+           This should be an integer between 1 and 7.")
+   }
+   if (!(k %in% k.range)) {
+      stop("Invalid value for k.
+           It should be an integer between 1 and 5.")
+   }
+   #
+   # initialize empty vector
+   labels <- c()
+   #
+   # always include z0 in the labels vector
+   labels <- c(labels, "z0")
+   #
+   # if k is 1, add additional labels for 1:1 for each value of i
+   if(k == 1){
+      for (ii in i){
+         labels <- c(labels, paste0("z", ii, 1:1))
+      }
+   }
+   # if k is 2, add additional labels for 1:2 for each value of i
+   if(k == 2){
+      for (ii in i){
+         labels <- c(labels, paste0("z", ii, 1:2))
+      }
+   }
+   # if k is 3, add additional labels for 1:3 for each value of i
+   if(k == 3){
+      for (ii in i){
+         labels <- c(labels, paste0("z", ii, 1:3))
+      }
+   }
+   # if k is 4, add additional labels for 1:4 for each value of i
+   if(k == 4){
+      for (ii in i){
+         labels <- c(labels, paste0("z", ii, 1:4))
+      }
+   }
+   # if k is 5, add additional labels for 1:5 for each value of i
+   if(k == 5){
+      for (ii in i){
+         labels <- c(labels, paste0("z", ii, 1:5))
+      }
+   }
+   #
+   # rename Z.mat columns
+   colnames(Z.mat) <- labels
+   #
+   # return Z.mat
    return(Z.mat)
 }
